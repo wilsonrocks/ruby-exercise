ac08ca70562cb9b94f3e5cf14e53da8313bcb15c
James Wilson <jwilsoncalderdale@gmail.com>
2018-09-01 16:00:16 +0100

    add tests and code for single job and multiple unordered jobs

--- jobs.rb
+++ jobs.rb
@@ -1,5 +1,9 @@
 def order_jobs (job_hash)
+
   if job_hash == '' then
     return []
   end
+
+  return job_hash.keys
+
 end
--- tc_jobs.rb
+++ tc_jobs.rb
@@ -1,11 +1,23 @@
 require_relative "jobs"
 require "test/unit"
-  
+
 class TestOrderJobs < Test::Unit::TestCase
 
   def test_empty_string
     assert_equal([], order_jobs(''))
   end
 
-  
+  def test_one_job
+    assert_equal(['a'], order_jobs({'a' => nil}))
+  end
+
+  def test_many_unordered_jobs
+    input = {
+      'a' => nil,
+      'c' => nil,
+      'b' => nil, #in strange order as test shouldn't rely on key order
+    }
+    assert_equal(['a', 'b', 'c'], order_jobs(input).sort)
+  end
+
 end
